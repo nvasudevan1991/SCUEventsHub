46a505ce6cdf11eb9dd634d1d687c7076195bd6b
nithi <nvasudevan@scu.edu>
2017-06-01 10:59:25 -0700

    Integrating two projects

--- FirstProjectApp/src/main/java/edu/scu/androidproject/MenuPage.java
+++ FirstProjectApp/src/main/java/edu/scu/androidproject/MenuPage.java
@@ -1,11 +1,11 @@
 package edu.scu.androidproject;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,7 +13,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import edu.scu.calendermonthviewtest.*;
 
 public class MenuPage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -93,6 +96,12 @@ public class MenuPage extends AppCompatActivity
             fragmentTransaction.replace(R.id.framelayout,fragment);
             fragmentTransaction.commit();
         } else if (id == R.id.events) {
+            EventFragment fragment =new EventFragment();
+            android.support.v4.app.FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.framelayout,fragment);
+            fragmentTransaction.commit();
+           Intent event=new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(event);
 
         } else if (id == R.id.about) {
             AboutFragment fragment =new AboutFragment();
