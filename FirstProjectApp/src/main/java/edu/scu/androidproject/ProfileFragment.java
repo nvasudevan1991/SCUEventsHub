09b4360ab004c28bbe6277fdbc39bc1c5566d7b2
nithi <nvasudevan@scu.edu>
2017-06-09 15:52:39 -0700

    Another List of Updates

--- FirstProjectApp/src/main/java/edu/scu/androidproject/ProfileFragment.java
+++ FirstProjectApp/src/main/java/edu/scu/androidproject/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -28,27 +29,35 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
     @Override
     public void onStart() {
-        List<String> catched =new ArrayList<>();
         super.onStart();
         Bundle bundle = getArguments();
 
-        if(bundle != null){
+        if (bundle != null) {
+            List<String> result = new ArrayList<>();
             stringValue = (String) bundle.getString("user_email");
-            DbHelper dbHelper=new DbHelper(getActivity());
-          // catched = dbHelper.onSearch(stringValue);
-            Toast.makeText(getActivity(),catched.get(0),Toast.LENGTH_LONG).show();
-
-
-
+            DbHelper db = new DbHelper(getActivity());
+            result = db.Select(stringValue);
+            String str = result.get(0);//User Name
+            TextView uname = (TextView) getActivity().findViewById(R.id.usernametextview);
+            uname.setText(str);
+            String str2 = result.get(1);//First Name
+            TextView fname =(TextView)getActivity().findViewById(R.id.firstnametextview);
+            fname.setText(str2);
+            String str3 = result.get(2);//Last Name
+            TextView lname =(TextView)getActivity().findViewById(R.id.lastnametextview);
+            lname.setText(str3);
+            String str4 = result.get(3);//Email
+            TextView email=(TextView)getActivity().findViewById(R.id.emailaddresstextview);
+            email.setText(str4);
+            Toast.makeText(getActivity(), str4, Toast.LENGTH_LONG).show();
             // String myString = bundle.containsKey("user_email") ? bundle.getString("user_email") : "hi, cannot find me";
-          //  TextView myAwesomeTextView = (TextView)getActivity().findViewById(R.id.textView2);
-         //   myAwesomeTextView.setText(stringValue);
+            //   TextView myAwesomeTextView = (TextView)getActivity().findViewById(R.id.textView2);
+            //   myAwesomeTextView.setText(stringValue);
         }
 
     }
