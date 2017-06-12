b58f6c262dc7f40dcf3844348663dd8b4e09e4a0
nithi <nvasudevan@scu.edu>
2017-06-12 02:02:41 -0700

    06/11 - Before Publishing again but added a fix for back out from events page

--- SecondApp/src/main/java/edu/scu/calendermonthviewtest/MainActivity.java
+++ SecondApp/src/main/java/edu/scu/calendermonthviewtest/MainActivity.java
@@ -23,8 +23,13 @@ public class MainActivity extends AppCompatActivity {
      //   toolbar = (Toolbar) findViewById(R.id.tool_bar);
         v.setAdapter(new CustomAdapter(getSupportFragmentManager(), getApplicationContext()));
         va = getIntent().getStringExtra("e_d");
-    }
 
+    }
+    @Override
+    public void onBackPressed() {
+        //Toast.makeText(getApplication(),"Back",Toast.LENGTH_LONG).show();
+        System.exit(0);
+    }
     private class CustomAdapter extends FragmentPagerAdapter {
        private String fragments [] = {"Month View","Map View"};
         public CustomAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
